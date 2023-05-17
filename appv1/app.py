8a15e372b03d123c0ff6f9bdd2650710e3d2204e
radonn24 <richardadonnell@gmail.com>
2023-05-16 21:44:18 -0500

    adding categories

--- appv1/app.py
+++ appv1/app.py
@@ -19,26 +19,3 @@ with open(os.path.join(feed_files_folder, "all-feeds.xml"), "w") as f:
 
     for outline in freshrss_root.findall(".//outline[@type='rss']"):
         f.write(ET.tostring(outline).decode().strip() + "\n")
-
-
-# # In the "feedly.opml" file, find the <body> tag. Under the <body> element, there are several <outline> elements. Print their text attribute contents of all <outline> elements.
-# tree = ET.ElementTree(ET.fromstring(feedly_opml_contents))
-# root = tree.getroot()
-
-# body = root.find("body")
-# outlines = body.findall("outline")
-
-# for index, outline in enumerate(outlines):
-#     outlines.sort(key=lambda x: x.get("text"))
-#     print(f"{index + 1}. {outline.get('text')}")
-
-# # In the "freshrss.opml.xml" file, find the <body> tag. Under the <body> element, there are several <outline> elements. Print their text attribute contents of all <outline> elements.
-# tree = ET.ElementTree(ET.fromstring(freshrss_opml_contents))
-# root = tree.getroot()
-
-# body = root.find("body")
-# outlines = body.findall("outline")
-
-# for index, outline in enumerate(outlines):
-#     outlines.sort(key=lambda x: x.get("text"))
-#     print(f"{index + 1}. {outline.get('text')}")
